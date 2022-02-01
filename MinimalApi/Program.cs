50a796ece934a1a0976d4913f02836a938dec7d0
Leo Migot <leonardomigot@hotmail.com>
2022-02-01 19:23:39 -0300

    feito crud

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -3,8 +3,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseInMemoryDatabase("Clientes"));
 
 var app = builder.Build();
 
@@ -14,7 +16,42 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseSwagger();
- 
+
+app.MapGet("/Clientes", async (AppDbContext dbContext) => 
+    await dbContext.Clientes.ToListAsync());
+
+app.MapGet("/Clientes/{id}", async (int id, AppDbContext dbContext) =>
+    await dbContext.Clientes.FirstOrDefaultAsync(app => app.Id == id));
+
+app.MapPost("/Clientes", async (Cliente cliente, AppDbContext dbContext) =>
+{ 
+    dbContext.Clientes.Add(cliente);
+    await dbContext.SaveChangesAsync();
+
+    return cliente;
+
+});
+
+app.MapPut("/Clientes/{id}", async (Cliente cliente, AppDbContext dbContext) =>
+{
+    dbContext.Entry(cliente).State = EntityState.Modified;
+    await dbContext.SaveChangesAsync();
+
+    return cliente;
+});
+
+app.MapDelete("/Clientes/{id}", async (int id, AppDbContext dbContext) =>
+{
+    var cliente = await dbContext.Clientes.FirstOrDefaultAsync(a => a.Id == id);
+
+    if (cliente != null)
+    {
+        dbContext.Clientes.Remove(cliente);
+        await dbContext.SaveChangesAsync();
+    }
+    return;
+});
+
 //app.MapGet("/cliente", () => new Cliente("Nome", "jo@a.o"));
 
 app.UseSwaggerUI();
